9cbe41bf87d8a5b9104d59c28c7574c76d4e0ee4
IsmaelCastV <ismaelcastaneda.v@gmail.com>
2023-04-23 18:29:47 -0500

    testing new invokeURL

--- Unicorn-OG-main/js/config.js
+++ Unicorn-OG-main/js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://8c0puu9lok.execute-api.us-west-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://8c0puu9lok.execute-api.us-west-2.amazonaws.com/prod'           // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
