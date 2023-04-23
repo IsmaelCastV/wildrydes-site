032129e9fd83c939fdec195cd0931c87c7f9b40f
IsmaelCastV <ismaelcastaneda.v@gmail.com>
2023-04-23 17:48:38 -0500

    new_config

--- Unicorn-OG-main/js/config.js
+++ Unicorn-OG-main/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId: 'us-west-2_V867PcNqL',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '56lqah7vanmfg9mj1lf984h0h7',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
